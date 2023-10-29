f9fc44d5db957a726764e0eff7ea0e985b495487
bhanu3450 <bhanu3450@gmail.com>
2023-10-29 11:27:34 +0530

    Assignment2 q7 done

---
+++ Assignment02_Q7/src/Queue.java
@@ -0,0 +1,34 @@
+
+public class Queue {
+	private int arr[];
+	private int front, rear;
+	private final int SIZE;
+
+	public Queue(int size) {
+		SIZE = size;
+		arr = new int[SIZE];
+		front = 0;
+		rear = 0;
+	}
+
+	public void push(int data) {
+		rear++;
+		arr[rear] = data;
+	}
+
+	public void pop() {
+		front++;
+	}
+
+	public int peek() {
+		return arr[front];
+	}
+
+	public boolean isFull() {
+		return rear == SIZE ;
+	}
+
+	public boolean isEmpty() {
+		return front == rear;
+	}
+}
